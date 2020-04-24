ff79ed75a45ed40b04f7803a702f34fb6069956c
Paal de Vibe <paal@menneskemaskin.no>
2020-04-24 12:50:57 +0200

    Add validate

---
+++ cicd/validate_quality.py
@@ -0,0 +1,14 @@
+from github import Github
+
+import os
+token = os.getenv('GITHUB_TOKEN')
+print("token:", repr(token))
+
+g = Github(token)
+
+org = g.get_organization('TeliaSoneraNorge')
+print('org', repr(org))
+repo = org.get_repo('dss_project_pr_experiment')
+print("repo:", repr(repo))
+pr = repo.get_pull(1)
+print("pr:", repr(pr))
